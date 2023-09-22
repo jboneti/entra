67b4b29a0def78c145fb53a366765cdcc460699d
jboneti <142340678+jboneti@users.noreply.github.com>
2023-09-22 19:04:20 -0300

    Add files via upload

---
+++ conteudo.java
@@ -0,0 +1,51 @@
+
+// public class conteudo 
+
+
+
+// *Classe*
+// Uma classe é um elemento do código Java que utilizamos para representar
+// objetos do mundo real.
+
+
+
+
+// *Matriz*
+// Uma matriz é basicamente um vetor onde cada elemento é por sua vez um vetor.
+// for(int j = 0; j < 9; j++) tab[i][j] = i*j; ...
+// Uma matriz é um vetor de vetores, todos de mesmo tamanho.
+
+
+
+// *String*
+// String não é um tipo primitivo, mas uma classe em Java que representa objetos
+// que são uma cadeia de caracteres.
+
+
+
+
+
+// *Tipo de vetores*
+//O Java possui dois tipos de dados que são divididos em por valor (tipos primitivos) e por referência (tipos por referência). 
+//Os tipos primitivos são boolean, byte, char, short, int, long, float e double. Os tipos por referência, são classes que especificam os tipos de objeto Strings, 
+//Arrays Primitivos e Objetos.
+
+
+
+// *vetor*
+//O que são vetores e como são utilizados no Java? Vetores são estruturas de dados que armazenam usualmente uma quantidade 
+//fixa de dados de um certo tipo; por esta razão, também são conhecidos como estruturas homogêneas de dados.
+
+
+
+// *Quais são os operadores lógicos em Java?*
+//Em Java os operadores lógicos utilizados são E (representado por &&) e OU (representado por ||). OBS. O símbolo do OU (I) corresponde a barra vertical ou pipe do teclado, 
+//caractere ASCII 124. Quando usamos os operadores duplos (&& ou ||) o Java não continua as operações se o resultado já for conhecido.
+
+
+
+// *Variavel*
+//Uma variável é uma área de memória, associada a um nome, que pode armazenar valores de um determinado tipo. Um tipo de dado define um conjunto de valores e um conjunto de operações. 
+//Java é uma linguagem com rigidez de tipos (em inglês strongly typed).
+
+

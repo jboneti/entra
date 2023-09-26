801e3cea0aef1336676f9a0626349cb484f504f2
jboneti <142340678+jboneti@users.noreply.github.com>
2023-09-25 21:44:21 -0300

    Add files via upload

---
+++ atividades/exercicios.java
@@ -0,0 +1,137 @@
+package atividades;
+
+public class exercicios {
+/*	Em uma classe denominada Pessoa, deseja-se manter o nome, sexo, data de nascimento e estado civil de uma pessoa.
+	O sexo deve ser definido como um caracter.
+	A data deve ser definida como String.
+
+	Crie 2 objetos de Pessoa.
+
+	--------------------------------------------------------------------------------------------------------------------
+
+	Crie uma classe denominada Mercado.
+
+	Essa classe terá 4 atributos, as informações que deverão ser guardadas são:
+	Número de maçãs vendidas por ano.
+	Preço de venda das maçãs.
+	Número de laranjas vendidas por ano.
+	Preço de venda das laranjas.
+
+	Crie 3 objetos da classe Mercado chamados:
+
+	unidadeDeBlumenau
+	unidadeDeJoinville
+	unidadeDeFlorianopolis
+
+	-----------------------------------------------------------------------------------------------------------------
+
+	A partir das representações abaixo dos objetos de uma classe Produto.
+	Escreva o código necessário para suportar tais objetos.
+	Ou seja, o código necessário para criar a classe e instanciar os objetos de modo que fiquem com o estado apresentado.
+
+	produto1: Produto
+	nome = “Caderno”
+	descricao = “Caderno em espiral tamanho médio”
+	precoUnitario = 4,50
+	desconto = 15
+
+
+	produto2: Produto
+	nome = “Caneta ESF”
+	descricao = “Caneta esferográfica 5mm”
+	precoUnitario = 1,20
+	desconto = 2
+
+
+	produto3: Produto 
+	nome = “Esquadro”
+	descricao = “Esquadro de acrílico 20 cm”
+	precoUnitario = 2,35
+	desconto = 10
+
+	-------------------------------------------------------------------------------------------------------
+
+	Crie uma classe chamada Circulo que tenha o atributo raio
+
+	Calcule a área de 5 objetos diferentes de Circulo
+
+	Para obter o valor de PI, use a função Math.PI do Java
+
+	----------------------------------------------------------------------------------------------------------
+
+	Utilizando a classe Mercado.
+
+	Atribua valores às variáveis da classe Mercado para cada um dos objetos de Mercado.
+
+	Sendo que o preço de compra dos produtos foi igual em todos os mercados.
+
+	Calcule:
+	a) Quem teve o maior lucro vendendo maçãs?
+	b) Quem teve o maior lucro vendendo laranjas?
+	c) Qual das lojas teve o maior lucro nesse ano específico?
+	d) Qual das lojas teve o menor lucro?
+	e) Qual das lojas ficou no "meio" em termos de lucro?
+	f) Juntando as 3 lojas, a franquia teve um lucro maior vendendo maçãs ou laranjas?
+
+	--------------------------------------------------------------------------------------------------------------------
+
+	O código a seguir foi escrito por um programador descuidado que, após tê-lo escrito, perdeu o código original da classe Cidade.
+	Verifique o trecho de código abaixo, e escreva qual seria o código necessário da classe.
+
+
+	Cidade gaspar = new Cidade();
+	gaspar.nome( "Gaspar" );
+	gaspar.uf( "SC" );
+	gaspar.populacaoMasculina( 28141 );
+	 
+	Cidade indaial = new Cidade();
+	indaial.nome( "Indaial" );
+	indaial.populacaoFeminina( 25844 );
+	indaial.calculado( true );
+
+	---------------------------------------------------------------------------------------------------
+
+	Faça uma classe chamada Aeronave.
+	Com os atributos:
+	Passageiros.
+	Velocidade máxima.
+	Capacidade de combustível.
+	Queima de combustível por minuto.
+
+	Crie 4 objetos de sua preferência.
+
+
+	Calcule:
+	a) Qual das aeronaves pode ficar no ar por mais tempo?
+	b) Qual aeronave leva o maior número de passageiros?
+	c) Considerando que os aviões estão em velocidade máxima, qual deles consegue voar mais longe?
+
+	-----------------------------------------------------------------------------------------------------------
+
+	Para efetuar o recolhimento do Imposto de Renda a Receita Federal tem o NOME, CPF, UF (RS, PR e SC) e RENDA ANUAL.
+	EX:
+	Nome: João da Silva
+	CPF: 123.456.789-00
+	UF: PR
+	RendaAnual: R$40.000
+
+	Para o cálculo do imposto a pagar de cada contribuinte, considere o seguinte:
+
+	Nível de Renda Anual     Alíquota
+	0 a 4.000                  0%
+	4.001 a 9.000              5,8%
+	9.001 a 25.000             15%
+	25.001 a 35.000            27,5%
+	acima de 35.000            30%
+
+
+	Sendo assim, deve-se calcular o imposto a pagar do seguinte modo:
+	Imposto a pagar = Renda Anual * Alíquota
+
+	Crie 5 objetos da classe Contribuinte e coloque-os em um vetor.
+
+	Calcule:
+	a) Quem mais paga imposto.
+	b) Quem menos paga imposto.
+	c) Qual o total de imposto pago entre os 5 contribuintes?*/
+}
